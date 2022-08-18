16b85ea2f7595bab910052d442f9af24d19865cb
JuliusMatadamas <julius3877@gmail.com>
2022-08-18 15:17:36 -0500

    Se crea el metodo para renderizar las vistas.

--- core/App.php
+++ core/App.php
@@ -5,8 +5,16 @@ namespace app\core;
 class App
 {
     public Router $router;
+    public Request $request;
+
     public function __construct()
     {
-        $this->router = new Router();
+        $this->request = new Request();
+        $this->router = new Router($this->request);
+    }
+
+    public function run()
+    {
+        echo $this->router->resolve();
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -4,7 +4,55 @@ namespace app\core;
 
 class Router
 {
-    public function __construct()
+    public Request $request;
+    protected array $routes = [];
+
+    /**
+     * Router constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
     {
+        $this->request = $request;
+    }
+
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath ();// Se obtiene la ruta
+        $method = $this->request->getMethod ();// Se obtiene el tipo de petición HTTP (get, post, put, patch, delete)
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if ($callback === false)
+        {
+            return "Not found";
+        }
+
+        // renderizar la vista
+        if (is_string ($callback))
+        {
+            return $this->renderView($callback);
+        }
+
+        return call_user_func ($callback);
+
+        /*echo "<pre>";
+        var_dump ($callback);
+        echo "</pre>";
+        exit;*/
+    }
+
+    /**
+     * Método para renderizar la vista pasada como párametro
+     * @param $view - string con el nombre la vista a renderizar
+     */
+    public function renderView($view)
+    {
+        include_once __DIR__."/../views/$view.php";
     }
 }
